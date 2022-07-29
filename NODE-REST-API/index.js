7330c2fb4a38cf4ea074e0bfb7ec4e0aae167196
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-30 01:29:30 +0530

    Creating User Router

--- NODE-REST-API/index.js
+++ NODE-REST-API/index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
 
 dotenv.config();
 
@@ -15,6 +18,10 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true, useUnifiedTopolog
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+app.use('/api/users',userRoute);
+app.use('/api/auth',authRoute);
+
 
 app.get('/',(req,res)=>{
     res.send('Welcome to Home Page')
