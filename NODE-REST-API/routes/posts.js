7d2014631296ad156f133bd47145deed56b5b373
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-09 01:28:33 +0530

    Node.js MongoDB Post CRUD

--- NODE-REST-API/routes/posts.js
+++ NODE-REST-API/routes/posts.js
@@ -1,5 +1,51 @@
 const router = require('express').Router();
+const Post = require("../models/Post")
 
 //create a post
+router.post("/", async (req,res)=>{
+    const newPost = new Post(req.body)
+    try {
+        const savePost = await newPost.save();
+        res.status(200).json(savePost);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//update apost
+router.put("/:id",async(req,res)=>{
+    try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+        await post.updateOne({$set:req.body})
+        res.status(200).json("the post has been updated")
+    }else{
+        res.status(403).json("You can update only your post")
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//delete a post
+router.delete("/:id",async(req,res)=>{
+    try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId){
+        await post.delteOne({$set:req.body})
+        res.status(200).json("the post has been deleted")
+    }else{
+        res.status(403).json("You can delete only your post")
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//like a post
+
+//get a post
+
+//get timeline posts
 
 module.exports = router;
