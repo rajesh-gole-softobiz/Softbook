b78bc874a23269a99d7d446736fd32b8e2b067ff
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-13 15:50:27 +0530

    Topbar Component

---
+++ react-softbook/src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import './topbar.css'
+import {Search, Person, Chat, Notifications} from '@mui/icons-material';
+
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+        <div className="topbarLeft">
+          <span className="logo">Softbook</span>
+        </div>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search/>
+            <input placeholder='Search for friend, post or video' className="searchInput" />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person/>
+              <div className="topbarIconBadge">1</div>
+            </div>
+
+            <div className="topbarIconItem">
+              <Chat/>
+              <div className="topbarIconBadge">2</div>
+            </div>
+
+            <div className="topbarIconItem">
+              <Notifications/>
+              <div className="topbarIconBadge">1</div>
+            </div>
+
+            <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+          </div>
+        </div>
+    </div>
+  )
+}
