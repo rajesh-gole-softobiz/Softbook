870972803e208bcb02e9cca989cf82787740dcfd
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-15 00:29:18 +0530

    Using React Router

--- react-softbook/src/App.js
+++ react-softbook/src/App.js
@@ -1,11 +1,30 @@
 import Home from "./pages/home/Home";
-// import Profile from "./pages/profile/Profile";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register"
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register"
+
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-     <Home/>
+     <Router>
+      <Switch>
+
+        <Route exact path="/" element={<Home/>}/>
+
+        <Route path="/login" element={<Login/>} />
+          
+        <Route path="/register" element={<Register/>} />   
+
+        <Route path="/profile/:username" element={<Profile/>} />
+          
+
+      </Switch>
+     </Router>
   );
 }
 
